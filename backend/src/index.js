97a3fdb01c6799dc2895640c1334de37af7855e0
Hadahamdaa <mochamadmocsya@gmail.com>
2025-06-09 14:38:06 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://new-chat-app-tau.vercel.app/",
     credentials: true,
   })
 );
